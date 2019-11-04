9a9a32f3dbfe7ec693b6853b5a7ed1542ab62c13
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-11-04 01:15:04 -0500

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Ricardo Herrera Petit on 11/4/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
