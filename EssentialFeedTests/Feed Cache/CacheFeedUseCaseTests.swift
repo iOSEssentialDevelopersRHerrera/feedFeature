ddf8bc3e5b0fe4cb3a5cd12a13bb0676d0edfd03
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-01-01 17:25:05 -0500

    Guarantee that the local feed loader does not deliver insertion error after instance has been deallocated

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -14,7 +14,10 @@ class LocalFeedLoader {
         store.deleteCachedFeed { [weak self ] error in
             guard let self = self else { return }
             if error == nil {
-                self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+                    guard self != nil else { return }
+                    completion(error)
+                }
             } else {
                 completion(error)
             }
@@ -103,18 +106,33 @@ class CacheFeedUseCaseTests: XCTestCase {
         
     }
     
-    func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
+    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
         var receivedResults = [Error?]()
         sut?.save([uniqueItem()]) { receivedResults.append($0) }
         
+       
+        store.completeDeletionSuccessfully()
         sut = nil
-        store.completeDeletion(with: anyNSError())
+        store.completeInsertion(with: anyNSError())
         
         XCTAssertTrue(receivedResults.isEmpty)
     }
+   
+    func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
+           let store = FeedStoreSpy()
+           var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+           
+           var receivedResults = [Error?]()
+           sut?.save([uniqueItem()]) { receivedResults.append($0) }
+           
+           sut = nil
+           store.completeDeletion(with: anyNSError())
+           
+           XCTAssertTrue(receivedResults.isEmpty)
+       }
     
     //MARK - Helpers
     
