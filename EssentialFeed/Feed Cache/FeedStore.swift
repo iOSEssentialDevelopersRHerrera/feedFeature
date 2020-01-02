8c4f737a9da413a96ec2b72817dfec7326678876
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-01-02 12:11:36 -0500

    Load command requests cache retrieval

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 public protocol FeedStore {
-     typealias DeletionCompletion = (Error?)-> Void
+    typealias DeletionCompletion = (Error?)-> Void
     typealias InsertionCompletion = (Error?)-> Void
     
-     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func retrieve()
 }
 
 
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -3,10 +3,19 @@ import EssentialFeed
 
 class LoadFeedFromCacheUseCaseTests: XCTestCase {
 
-     func test_init_doesNotMessageStoreUponCreation() {
-           let (_, store) =  makeSUT()
-           XCTAssertEqual(store.receivedMessages, [])
-       }
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) =  makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func  test_load_requestsCacheRetrieval() {
+        let (sut, store) =  makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+        
+    }
 
     //MARK - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,  file: StaticString = #file, line:UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
